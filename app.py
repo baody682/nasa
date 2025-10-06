1eba1392c95855fba591fb1f6c32ebd69e3b1159
baody682 <bodymahboub.eg@gmail.com>
2025-10-06 05:45:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 STATIC_PLOT_FILENAME = 'hotspot_globe_npp_strict_mask.html' 
 STATIC_PLOT_PATH = 'images/' + STATIC_PLOT_FILENAME
 
+STATIC_PLOT_FILENAME_2 = 'hotspot_globe_3d.html' 
+STATIC_PLOT_PATH_2 = 'images/' + STATIC_PLOT_FILENAME_2
+
 os.makedirs('static/images', exist_ok=True) 
 
 app = Flask(__name__)
@@ -19,8 +22,12 @@ def tag_details():
 
 @app.route('/mathematical_model')
 def mathematical_model():
-    hotspot_globe_url = url_for('static', filename=STATIC_PLOT_PATH) 
-    return render_template('model.html')
+    hotspot_globe_url_value = url_for('static', filename=STATIC_PLOT_PATH_2)  
+    return render_template('model.html', hotspot_globe_3d=hotspot_globe_url_value)
+
+@app.route('/research_details')
+def research_details():
+    return render_template('research.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
